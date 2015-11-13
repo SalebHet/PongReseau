0eca3c711b11fc5e14c088f80e01ce5542997fa8
Clement <clement.nerestan@gmail.com>
2015-11-13 09:25:39 +0100

    modification du constructeur

--- src/Racket.java
+++ src/Racket.java
@@ -1,8 +1,9 @@
 
 public class Racket extends PongItem{
 	
-	public Racket (String Image,  int posX, int posY){
+	public Racket (String Image,  int posX, int posY, int speedY){
 		super(Image, posX, posY);
+		this.speedY = speedY;
 	}
 
 }
