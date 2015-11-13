ca9bffa58b25aa263a524adf9180c3eef41082a0
emeryk <mr.emeryk@gmail.com>
2015-11-13 10:43:29 +0100

    ajout KeyReleased

--- src/Pong.java
+++ src/Pong.java
@@ -62,7 +62,19 @@ public class Pong {
 		}
 	}
 	
-	
-	
+	public void keyPressed (KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+		case KeyEvent.VK_KP_UP:
+			Racket.setSpeedY(0);
+			break;
+		case KeyEvent.VK_DOWN:
+		case KeyEvent.VK_KP_DOWN:
+			Racket.setSpeedY(0);
+			break;
+		default:
+			System.out.println("got press "+e);
+	}
+	}
 	
 }
